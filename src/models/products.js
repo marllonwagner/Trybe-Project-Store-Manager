824b21584ca58edfdfb5811a8edb7fce6b090f45
marllonwagner <marllonwagner91@gmail.com>
2023-03-21 16:40:12 -0300

    task 1 done

--- src/models/products.js
+++ src/models/products.js
@@ -1,9 +1,17 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.products;';
+  const query = 'SELECT * FROM StoreManager.products ORDER BY id;';
   const [products] = await connection.execute(query);
   return products;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
+  const [[product]] = await connection.execute(query, [id]);
+  return product;
+};
+
+module.exports = {
+  getAll,
+  getById };
