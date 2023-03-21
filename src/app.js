1c1266347e73330ec0b4359065aee7fdd212c2d6
marllonwagner <marllonwagner91@gmail.com>
2023-03-21 15:35:51 -0300

    getAll controller create

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const productsController = require('./controllers/products');
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products/', productsController.getAll);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
---
+++ src/controllers/products.js
@@ -0,0 +1,12 @@
+const productsService = require('../services/products');
+
+const getAll = async (_req, res, next) => {
+try {
+  const products = await productsService.getAll();
+res.status(200).json(products);
+} catch (error) {
+  next(error);
+}
+};
+
+module.exports = getAll;
