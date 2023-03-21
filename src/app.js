07dc1a8fd37df5c66816c1fa8fb11c23385fee4d
marllonwagner <marllonwagner91@gmail.com>
2023-03-21 16:19:30 -0300

    error handler create

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const productsController = require('./controllers/products');
+const errorHandler = require('./middlewares/errorHandler');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -13,6 +14,7 @@ app.get('/', (_request, response) => {
 
 app.get('/products', productsController.getAll);
 
+app.use(errorHandler);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
