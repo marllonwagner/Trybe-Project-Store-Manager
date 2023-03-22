9e1effb53b01935e8fce1db0ef9aa15ec0a19912
marllonwagner <marllonwagner91@gmail.com>
2023-03-22 20:35:37 -0300

    task 6

--- src/middlewares/validateProdId.js
+++ src/middlewares/validateProdId.js
@@ -1,25 +1,22 @@
 const connection = require('../models/connection');
 
-const httpErrGenerator = (status, message) => ({ status, message });
-
 const getAllIds = async () => {
   const query = 'SELECT product_id FROM StoreManager.sales_products;;';
   const [productsIds] = await connection.execute(query);
-  return productsIds;
+  const idsArr = productsIds.map((product) => product.product_id);
+  return idsArr;
 };
 
-const isProdIdValid = (req, _res, next) => {
+const isProdIdValid = async (req, res, next) => {
   const salesArray = req.body;
-  const ids = getAllIds();
+  const ids = await getAllIds();
   for (let index = 0; index < salesArray.length; index += 1) {
     if (!(salesArray[index].productId in ids)) {
-      throw httpErrGenerator(404, 'Product not found');
+      return res.status(404).json({ message: 'Product not found' });
     }
   }
   
   return next();
 };
-
-// !(getById(salesArray[index].productId))
 
 module.exports = isProdIdValid;
